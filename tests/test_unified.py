e8e17245b705573c3ca6c4caaeec2d321a23f4f6
Daniele Rugginenti <dany1980@gmail.com>
2026-02-08 14:18:22 -0500

    test

--- tests/test_unified.py
+++ tests/test_unified.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 if __name__ == '__main__':
     config_overrides = {
-        'USER_FOLDER': '/home/dany/test_scrape_12',
+        'USER_FOLDER': '/home/dany/test_scrape_13',
         'POOLS': 32,
         'ASYNC_BLOCK_SIZE': 32,
         'MAXIMUM_RETRIES': 0,
@@ -14,7 +14,17 @@ if __name__ == '__main__':
         'LOG_LEVEL': 'DEBUG',
         'CRAWL_METHODS': ['robots', 'sitemaps'],
         'TIMEOUT': 30,
-        'RESUME': True
+        'RESUME': True,
+        # SEO modular checks
+        'SEO_CHECKS_ENABLED': True,
+        # If empty, all available checks are enabled
+        'SEO_ENABLED_CHECKS': [],
+        # Explicitly disable one or more checks
+        'SEO_DISABLED_CHECKS': [],
+        'SEO_H1_MAX_CHARS': 70,
+        'INCLUDED_EXPRESSIONS_URL': [
+            r'^(?!https?://[^/]+/2026/02/08/.*'
+        ],
     }
 
     # df = pd.read_csv('t.csv')
@@ -22,16 +32,16 @@ if __name__ == '__main__':
     
     # df = df.sample(n=10, random_state=42)  # random_state for reproducibility
 
-    doms = ['1cruisersboulevard.place', '1greatdealauctions.com', '1215design.com', '123nowitsclean.com', '1800waterdamage.com', 
-    'myshopify.com', 'my1stchoicedetail.com', 'mobiledetailingnampa.com', '1stchoiceweld.com', 'masonryboise.com', 'msha.ke', 
-    'cyrusramsey.com', 'glossgenius.com', 'martinconstructionboise.com', '208performancetuning.com', '208roofing.com', '208tees.com', 
-    'myshopify.com', '208taxhelp.com', '21apparel.com', '22northsalon.com', '2csignshop.com', 'apexintegratedsecurity.com', 'https://goagainst.com/', 'gitea.io']
+    # doms = ['1cruisersboulevard.place', '1greatdealauctions.com', '1215design.com', '123nowitsclean.com', '1800waterdamage.com', 
+    # 'myshopify.com', 'my1stchoicedetail.com', 'mobiledetailingnampa.com', '1stchoiceweld.com', 'masonryboise.com', 'msha.ke', 
+    # 'cyrusramsey.com', 'glossgenius.com', 'martinconstructionboise.com', '208performancetuning.com', '208roofing.com', '208tees.com', 
+    # 'myshopify.com', '208taxhelp.com', '21apparel.com', '22northsalon.com', '2csignshop.com', 'apexintegratedsecurity.com', 'https://goagainst.com/', 'gitea.io']
 
     # doms = ['apexintegratedsecurity.com', 'https://goagainst.com/', 'gitea.io']
     # doms = ['gitea.io']
 
     # doms = ['deskydoo.com']
-    # doms = ['1800waterdamage.com']
+    doms = ['elmomentoqroo.mx']
     # doms = []
     with ISpider(domains=doms, **config_overrides) as spider:
         spider.run()
